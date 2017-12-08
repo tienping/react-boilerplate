854c41b4f78cd70d5c1012258e5f10275feec175
TienPing - Mac Pro <petwesley@gmail.com>
2017-12-08 16:28:30 +0800

    indentation fix for homepage

--- app/containers/HomePage/tests/index.test.js
+++ app/containers/HomePage/tests/index.test.js
@@ -6,12 +6,16 @@ import HomePage from '../index';
 import messages from '../messages';
 
 describe('<HomePage />', () => {
-  it('should render the page message', () => {
-    const renderedComponent = shallow(
-      <HomePage />
-    );
-    expect(renderedComponent.contains(
-      <FormattedMessage {...messages.header} />
-    )).toEqual(true);
-  });
+    it('should render the page message', () => {
+        const renderedComponent = shallow(
+            <HomePage />
+        );
+        expect(renderedComponent.contains(
+            <div className="container">
+                <h1>
+                    <FormattedMessage {...messages.header} />
+                </h1>
+            </div>
+        )).toEqual(true);
+    });
 });
