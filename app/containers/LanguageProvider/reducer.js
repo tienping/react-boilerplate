85a1b2d8df6c33d6cf1be671972f7abd2587a3da
TienPing - Mac Pro <petwesley@gmail.com>
2017-12-08 16:27:42 +0800

    indentation fix for languageprovider

--- app/containers/LanguageProvider/reducer.js
+++ app/containers/LanguageProvider/reducer.js
@@ -7,24 +7,24 @@
 import { fromJS } from 'immutable';
 
 import {
-  CHANGE_LOCALE,
+    CHANGE_LOCALE,
 } from './constants';
 import {
-  DEFAULT_LOCALE,
+    DEFAULT_LOCALE,
 } from '../App/constants'; // eslint-disable-line
 
 const initialState = fromJS({
-  locale: DEFAULT_LOCALE,
+    locale: DEFAULT_LOCALE,
 });
 
 function languageProviderReducer(state = initialState, action) {
-  switch (action.type) {
-    case CHANGE_LOCALE:
-      return state
-        .set('locale', action.locale);
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case CHANGE_LOCALE:
+            return state
+                .set('locale', action.locale);
+        default:
+            return state;
+    }
 }
 
 export default languageProviderReducer;
