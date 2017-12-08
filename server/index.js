cfa8adf37eb52cb3559b94f04d42998e0114ef4a
TienPing - Mac Pro <petwesley@gmail.com>
2017-12-08 15:32:53 +0800

    indentation

--- server/index.js
+++ server/index.js
@@ -16,8 +16,8 @@ const app = express();
 
 // In production we need to pass these values in instead of relying on webpack
 setup(app, {
-  outputPath: resolve(process.cwd(), 'build'),
-  publicPath: '/',
+    outputPath: resolve(process.cwd(), 'build'),
+    publicPath: '/',
 });
 
 // get the intended host and port number, use localhost and port 3000 if not provided
@@ -27,20 +27,20 @@ const prettyHost = customHost || 'localhost';
 
 // Start your app.
 app.listen(port, host, (err) => {
-  if (err) {
-    return logger.error(err.message);
-  }
+    if (err) {
+        return logger.error(err.message);
+    }
 
   // Connect to ngrok in dev mode
-  if (ngrok) {
-    ngrok.connect(port, (innerErr, url) => {
-      if (innerErr) {
-        return logger.error(innerErr);
-      }
+    if (ngrok) {
+        ngrok.connect(port, (innerErr, url) => {
+            if (innerErr) {
+                return logger.error(innerErr);
+            }
 
-      logger.appStarted(port, prettyHost, url);
-    });
-  } else {
-    logger.appStarted(port, prettyHost);
-  }
+            logger.appStarted(port, prettyHost, url);
+        });
+    } else {
+        logger.appStarted(port, prettyHost);
+    }
 });
